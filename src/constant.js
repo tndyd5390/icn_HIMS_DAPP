d4317fdd077272f60b2b8e46436372abb2b9c6e5
tndyd5390 <tndyd5390@naver.com>
2019-04-24 20:47:37 +0900

    test

---
+++ src/constant.js
@@ -0,0 +1,5 @@
+const IP = "http://39.115.19.149:9999";
+
+module.exports = {
+    IP
+}
--- src/screen/InquiryHealthInfo.js
+++ src/screen/InquiryHealthInfo.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react';
+import { IP } from "../constant";
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    FlatList
 } from 'react-native';
 
-export default class InquiryHealthInfo extends Component {
+class UserItem extends Component {
+    _onPress = () => {
+        this.props.onPressItem(this.props.key);
+    }
     render() {
         return(
-            <View>
-                <Text>건강정보 어쩌구 저쩌구</Text>
-            </View>
+            <TouchableOpacity onPress={this._onPress}>
+                <View>
+                    <Text style={{color: textColor}}>{this.props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+export default class InquiryHealthInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userList : []
+        }
+    }
+    _keyExtractor = (item, index) => item.id;
+
+    _onPressItem = (id) => {
+        // updater functions are preferred for transactional updates
+        this.setState((state) => {
+          // copy the map rather than modifying state.
+          const selected = new Map(state.selected);
+          selected.set(id, !selected.get(id)); // toggle
+          return {selected};
+        });
+      };
+    
+
+    _getUserList = () => {
+        fetch(IP + "/getAllUsers", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+        .then((response) => response.json())
+        .then((res) => {
+            this.setState({userList: res});
+        })
+    }
+
+    _renderItem = ({item}) => (
+        <UserItem
+          id={item.key}
+          onPressItem={this._onPressItem}
+          selected={!!this.state.selected.get(item.id)}
+          title={item.title}
+        />
+      );
+
+    render() {
+        setInterval(
+            this._getUserList, 2000
+        )
+        
+        return(
+            <FlatList
+                data={this.props.data}
+                extraData={this.state}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderItem}
+            />
         )
     }
 }
